9e62e1bc16f5ea1b357ed6975faffe027beb96ea
Eyad Sibai <eyad.alsibai@gmail.com>
2015-10-20 11:14:57 +0200

    update rep

--- myml/utils.py
+++ myml/utils.py
@@ -1,30 +1,25 @@
 import pandas as pd
 
 
-def convert_column_of_lists_to_df(column: pd.Series, prefix='', prefix_sep=''):
+def convert_series_of_lists_to_df(column: pd.Series, prefix='', prefix_sep=''):
     """
+    input:
 
-    :param df:
-    :param column_name:
-    :return:
-
-            index groups
+     index    groups
         0     ['a','b','c']
         1     ['c']
         2     ['b','c','e']
         3     ['a','c']
         4     ['b','e']
 
+    output:
 
-        output
-
-        index  a   b   c   d   e
-        0      1   1   1   0   0
-        1      0   0   1   0   0
-        2      0   1   1   0   1
-        3      1   0   1   0   0
-        4      0   1   0   0   0
-
+    index   a   b   c   d   e
+        0   1   1   1   0   0
+        1   0   0   1   0   0
+        2   0   1   1   0   1
+        3   1   0   1   0   0
+        4   0   1   0   0   0
     """
 
     return pd.get_dummies(column.apply(pd.Series), prefix=prefix, prefix_sep=prefix_sep).sum(level=0, axis=1)
