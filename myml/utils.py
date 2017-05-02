6124f1357bad7d681940934a2cfc000534723213
Eyad Sibai <eyad.alsibai@gmail.com>
2017-05-02 15:14:38 +0200

    add plot func

--- myml/utils.py
+++ myml/utils.py
@@ -23,3 +23,20 @@ def convert_series_of_lists_to_df(column: pd.Series, prefix='', prefix_sep=''):
     """
 
     return pd.get_dummies(column.apply(pd.Series), prefix=prefix, prefix_sep=prefix_sep).sum(level=0, axis=1)
+
+
+def plot_features_ditributions(df: pd.DataFrame, target_column: str):
+
+    # Plotting the histogram of every feature per class (0 = no fraud, 1 = fraud)
+    columns = sorted(col for col in df.columns if col != target_column)
+    width = 2 # int(np.sqrt(len(columns)))
+    height = np.ceil(len(columns) / width)
+    with plt.rc_context(rc={'figure.figsize': (10, 25)}):
+        fig = plt.figure()
+        for col_i, col in enumerate(columns):
+            fig.add_subplot(height, width, col_i + 1)
+            value_range = (df[col].quantile(.01), df[col].quantile(.99))
+            # TODO find a way get a good bin numbers check brute force plotter
+            df.groupby(target_column)[col].plot.hist(alpha=0.5, normed=True, bins=20, range=value_range)
+            plt.title("Feature '{}'".format(col), fontdict=dict(fontsize=10))
+    plt.tight_layout()
